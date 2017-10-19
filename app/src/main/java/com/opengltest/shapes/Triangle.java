26406c24856956eeac1bcc88a3cd895c8c2ec7ef
nikskubak <nikskubak@gmail.com>
2017-10-19 18:52:01 +0300

    rotating shape

--- app/src/main/java/com/opengltest/shapes/Triangle.java
+++ app/src/main/java/com/opengltest/shapes/Triangle.java
@@ -9,11 +9,11 @@ import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
 public class Triangle {
-    private final String vertexShaderCode =
-            "attribute vec4 vPosition;" +
-                    "void main() {" +
-                    "  gl_Position = vPosition;" +
-                    "}";
+//    private final String vertexShaderCode =
+//            "attribute vec4 vPosition;" +
+//                    "void main() {" +
+//                    "  gl_Position = vPosition;" +
+//                    "}";
 
     private final String fragmentShaderCode =
             "precision mediump float;" +
@@ -21,6 +21,21 @@ public class Triangle {
                     "void main() {" +
                     "  gl_FragColor = vColor;" +
                     "}";
+
+    private final String vertexShaderCode =
+            // This matrix member variable provides a hook to manipulate
+            // the coordinates of the objects that use this vertex shader
+            "uniform mat4 uMVPMatrix;" +
+                    "attribute vec4 vPosition;" +
+                    "void main() {" +
+                    // the matrix must be included as a modifier of gl_Position
+                    // Note that the uMVPMatrix factor *must be first* in order
+                    // for the matrix multiplication product to be correct.
+                    "  gl_Position = uMVPMatrix * vPosition;" +
+                    "}";
+
+    // Use to access and set the view transformation
+    private int mMVPMatrixHandle;
 
     private FloatBuffer vertexBuffer;
     private final int mProgram;
@@ -96,6 +111,20 @@ public class Triangle {
 
         // Set color for drawing the triangle
         GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+
+        // Draw the triangle
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+
+        // Disable vertex array
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+    }
+
+    public void draw(float[] mvpMatrix) { // pass in the calculated transformation matrix
+        // get handle to shape's transformation matrix
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+
+        // Pass the projection and view transformation to the shader
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
 
         // Draw the triangle
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
